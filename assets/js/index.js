7cdbc176b01c05946a0eaa75342fac1ecac2de4a
diegoguerraf <guerra.farias.diego@gmail.com>
2024-02-25 23:32:58 -0300

    cambios de nav, footer, movil

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,23 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var navbar = document.querySelector('.navbar');
+    if (window.scrollY > 0) {
+        navbar.classList.add('navbar-scrolled');
+    } else {
+        navbar.classList.add('navbar-transparent');
+    }
+});
+
+// JavaScript para cambiar la clase del navbar al hacer scroll
+window.addEventListener('scroll', function() {
+    var navbar = document.querySelector('.navbar');
+    if (window.scrollY > 0) {
+        navbar.classList.add('navbar-scrolled');
+        navbar.classList.remove('navbar-transparent');
+    } else {
+        navbar.classList.remove('navbar-scrolled');
+        navbar.classList.add('navbar-transparent');
+    }
+});
 $(document).ready(function(){
     $("a").on('click', function(event) {
         if (this.hash !== "") {
