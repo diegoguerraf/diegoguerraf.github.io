b8c5cf38ce231c65c1bbf1c39267f15b652e1ad3
diegoguerraf <guerra.farias.diego@gmail.com>
2024-02-26 20:49:28 -0300

    cambios tooltips y correcciones nav

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,9 @@
+// Espera a que el DOM esté completamente cargado antes de ejecutar el código
 document.addEventListener('DOMContentLoaded', function() {
+    // Selecciona el navbar
     var navbar = document.querySelector('.navbar');
+    // Si el usuario ha hecho scroll hacia abajo, añade la clase 'navbar-scrolled'
+    // De lo contrario, añade la clase 'navbar-transparent'
     if (window.scrollY > 0) {
         navbar.classList.add('navbar-scrolled');
     } else {
@@ -7,9 +11,12 @@ document.addEventListener('DOMContentLoaded', function() {
     }
 });
 
-// JavaScript para cambiar la clase del navbar al hacer scroll
+// Agrega un evento de escucha para detectar el scroll de la ventana
 window.addEventListener('scroll', function() {
+    // Selecciona el navbar
     var navbar = document.querySelector('.navbar');
+    // Si el usuario ha hecho scroll hacia abajo, añade la clase 'navbar-scrolled' y elimina 'navbar-transparent'
+    // De lo contrario, elimina 'navbar-scrolled' y añade 'navbar-transparent'
     if (window.scrollY > 0) {
         navbar.classList.add('navbar-scrolled');
         navbar.classList.remove('navbar-transparent');
@@ -18,6 +25,8 @@ window.addEventListener('scroll', function() {
         navbar.classList.add('navbar-transparent');
     }
 });
+
+// jQuery para desplazarse suavemente a través de las secciones cuando se hace clic en los enlaces de la barra de navegación
 $(document).ready(function(){
     $("a").on('click', function(event) {
         if (this.hash !== "") {
@@ -30,4 +39,9 @@ $(document).ready(function(){
             });
         }
     });
+});
+
+// Inicializa los tooltips para los enlaces que tienen el atributo data-toggle="tooltip"
+$(document).ready(function(){
+    $('[data-toggle="tooltip"]').tooltip();
 });
